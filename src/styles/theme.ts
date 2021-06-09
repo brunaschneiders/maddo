92d32500973d7083a9d09901d7c2434853e68ffe
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-06-08 22:17:35 -0300

    add global styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,9 +3,12 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   colors: {
     gray: {
-      '800': '#41414C',
-      '50': '#E5E5E5'
-    }
+      '50': '#E5E5E5',
+      '400': '#737380',
+      '800': '#35353D'
+    },
+    orange: { '50': '#EDDFE2', '100': '#F4BFC9', '500': '#FF4916' },
+    pink: { '500': '#FE25B0' }
   },
   fonts: {
     heading: 'Roboto',
