04fe0b944d5e696d20d1e764b7c761330db81024
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-06-09 20:51:08 -0300

    fix storybook configs to get chakra styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { withNextRouter } from 'storybook-addon-next-router';
-import {addDecorator} from '@storybook/react'
-import {ChakraProvider} from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../src/styles/theme'
 
 export const parameters = {
   layout: 'fullscreen',
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: '#E5E5E5',
+      },
+    ],
+  },
+}
 
-};
+const withChakra = (StoryFn) => {
 
-export const decorators = [withNextRouter()]
+  return (
+    <ChakraProvider theme={theme}>
+      <StoryFn />
+    </ChakraProvider>
+  )
+}
 
-addDecorator(storyFn => (
-  <ChakraProvider resetCSS theme={theme}>
-    {storyFn()}
-  </ChakraProvider>
-))
+export const decorators = [withChakra, withNextRouter]
+
+
