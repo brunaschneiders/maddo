c12fbbf71b26941c5e859dd2affeb2f39af4d99c
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-06-08 22:22:40 -0300

    add storybook-addon-next-router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { withNextRouter } from 'storybook-addon-next-router';
 import {addDecorator} from '@storybook/react'
 import {ChakraProvider} from '@chakra-ui/react'
+
 import theme from '../src/styles/theme'
 
+export const decorators = [withNextRouter()]
 addDecorator(storyFn => (
   <ChakraProvider resetCSS theme={theme}>
     {storyFn()}
