f2c7bfce83ac18d55fb80a5575db9cb451717829
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-06-08 22:17:02 -0300

    fix storybook configs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
+import React from 'react'
+import {addDecorator} from '@storybook/react'
+import {ChakraProvider} from '@chakra-ui/react'
+import theme from '../src/styles/theme'
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+addDecorator(storyFn => (
+  <ChakraProvider resetCSS theme={theme}>
+    {storyFn()}
+  </ChakraProvider>
+))
